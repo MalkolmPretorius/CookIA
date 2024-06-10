311217efcbe3f6186dbe4863fe689bef3471ba72
usernamedlo <loic.ghijselings@usernamedlo.com>
2024-06-10 15:13:43 +0200

    Remove back folder

--- Back/resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-500 text-black focus:border-red-500 focus:ring-red-500 rounded-md shadow-sm']) !!}>
